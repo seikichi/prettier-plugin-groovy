119e17b5e7781fedc1f3ed406eab8e0c9f41fc5b
Seiichi KONDO <seikichi@kmc.gr.jp>
2019-06-30 18:16:53 +0900

    Add index.ts etc.

---
+++ src/index.ts
@@ -0,0 +1,67 @@
+import {
+  AST,
+  Doc,
+  FastPath,
+  Parser,
+  ParserOptions,
+  Printer,
+  SupportLanguage,
+} from "prettier";
+import { ANTLRInputStream, CommonTokenStream } from "antlr4ts";
+import { GroovyLexer } from "./antlr4/GroovyLexer";
+
+function parse(
+  text: string,
+  _parsers: { [parserName: string]: Parser },
+  _options: ParserOptions
+): AST {
+  const stream = new ANTLRInputStream(text);
+  const lexer = new GroovyLexer(stream);
+  const tokens = new CommonTokenStream(lexer);
+
+  // Debug
+  while (true) {
+    try {
+      tokens.consume();
+    } catch (e) {
+      break;
+    }
+  }
+  console.log();
+  for (const t of tokens.getTokens()) {
+    console.log(`${t.line}: ${t.text}`);
+  }
+
+  return {};
+}
+
+function printGroovy(
+  _path: FastPath<any>,
+  _options: ParserOptions,
+  _print: (path: FastPath<any>) => Doc
+): Doc {
+  return "";
+}
+
+export const languages: SupportLanguage[] = [
+  {
+    extensions: [".groovy"],
+    name: "GROOVY",
+    parsers: ["groovy-parse"]
+  } as SupportLanguage
+];
+
+export const parsers: { [parserName: string]: Parser } = {
+  "groovy-parse": {
+    parse,
+    astFormat: "groovy-ast",
+    locStart: _node => 0,
+    locEnd: _node => 0
+  }
+};
+
+export const printers: { [astFormat: string]: Printer } = {
+  "groovy-ast": {
+    print: printGroovy
+  }
+};
